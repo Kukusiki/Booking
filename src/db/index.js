ee5695f0e3ebe9aa7b4ae4449fa4128a0e4088eb
Kristina Ivanova <kristinkus0115@gmail.com>
2021-11-22 14:25:14 +0300

    change pass

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,9 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('booking', 'root', '011570', {
+const sequelize = new Sequelize('booking', 'root', '1111', {
     host: '127.0.0.1',
     port: '3306',
     dialect: 'mysql'
 });
 
 module.exports = sequelize;
-//console.log('123');
