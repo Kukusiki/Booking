3b0cebca303ec31983c40f599f8f7e1fa2244550
Kristina Ivanova <kristinkus0115@gmail.com>
2022-01-16 23:06:00 +0300

    Add loader

---
+++ src/loader/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+
+
+const bookingRouter = require('../routes/bookingRouter');
+const hotelRouter = require('../routes/hotelRouter');
+const reviewRouter = require('../routes/reviewRouter');
+const roleRouter = require('../routes/roleRouter');
+const roomRouter = require('../routes/roomRouter');
+const userInfoRouter = require('../routes/userInfoRouter');
+const userRoleRouter = require('../routes/userRoleRouter');
+const userRouter = require('../routes/userRouter');
+
+
+router.use('/bookings', bookingRouter);
+router.use('/hotels', hotelRouter);
+router.use('/reviews', reviewRouter);
+router.use('/roles', roleRouter);
+router.use('/rooms', roomRouter);
+router.use('/userInfos', userInfoRouter);
+router.use('/userRoles', userRoleRouter);
+router.use('/users', userRouter);
+
+module.exports = router;
