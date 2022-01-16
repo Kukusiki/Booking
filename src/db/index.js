971cde0ef6b883e7d7cc3e1f691dcd875e4d1a12
Kristina Ivanova <kristinkus0115@gmail.com>
2022-01-16 21:39:52 +0300

    fix router

--- src/db/index.js
+++ src/db/index.js
@@ -11,5 +11,4 @@ const sequelize = new Sequelize(
     }
 );
 
-sequelize.sync();
 module.exports = sequelize;
---
+++ src/routes/hotelRouter.js
@@ -0,0 +1,21 @@
+const hotelController = require('../controllers/hotelController');
+const admin = require('../middleware/admin');
+const user = require('../middleware/user');
+
+const express = require('express');
+const router = express.Router();
+const tryCatch = require('../utils/tryCatch');
+
+
+//router.use(user);
+router.get('/:id', tryCatch(hotelController.getHotelById));
+router.get('/', tryCatch(hotelController.getAllHotels));
+router.get('/:id/rooms', tryCatch(hotelController.getRoomsByHotelId));
+router.get('/:id/freeRooms', tryCatch(hotelController.getFreeRoomsByHotelId));
+router.get('/:id/reviews', tryCatch(hotelController.getReviewsByHotelId));
+
+//router.use(admin);
+router.post('/', tryCatch(hotelController.addHotel));
+router.delete('/:id', tryCatch(hotelController.deleteHotel));
+
+module.exports = router;
