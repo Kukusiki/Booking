6d9704e15b8373713c38d0ca7bcc28c0b7540aec
Kristina Ivanova <kristinkus0115@gmail.com>
2022-03-04 16:51:32 +0300

    Add automatic deletion of hotels

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const sequelize = require('./db');
 const routes = require('./loader');
 const seeds = require('./utils/seeds');
 const consts = require('./const');
+const hotelService = require('./services/hotelService');
 const express = require('express');
 const app = express();
 const env = require('./env').admin;
@@ -13,7 +14,18 @@ async function start() {
     await seeds.addAdmin({ email: env.email, password: env.password })
     app.use(routes);
 
+
     app.listen(5000, () => console.log('Connection successful, to exit press ctrl + c'));
+
+    let now = new Date();
+    let time = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 2, 0, 0, 0) - now;
+    if (time < 0) {
+        time += 24 * 60 * 60 * 1000; // it's after 2am, try 2am tomorrow.
+    }
+    setTimeout(async function() {
+        await hotelService.getOldHotels();
+        time += 24 * 60 * 60 * 1000;
+    }, time);
 }
 
 start();
